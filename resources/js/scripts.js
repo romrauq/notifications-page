01eae340c33e391f39bc46c000d0b0fc42186b01
romrauq <johnequarmor@gmail.com>
2025-01-21 18:55:47 +0000

    Added scripts to modify the background color of active notifications when clear button is clicked.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,12 +1,17 @@
 let notifications_count = document.getElementById("notifications-count");
 let clear_button = document.getElementById("clear-button");
 let notification_badges = document.getElementsByClassName("notification-badge");
+let active_notifications = document.getElementsByClassName("active-item");
 
 // Add event listener to clear button element:
 clear_button.addEventListener("click", () => {
 	for (let index = 0; index < notification_badges.length; index++) {
 		const item = notification_badges[index];
 		item.style.display = "none";
+	}
+	for (let index = 0; index < active_notifications.length; index++) {
+		const item = active_notifications[index];
+		item.style.backgroundColor = "white";
 	}
 	notifications_count.innerHTML = "0";
 });
